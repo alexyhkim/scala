1b1a8fd7de9e9ebebd83fe54bf4098b941925eb9
Alex Kim <akim@marinsoftware.com>
2015-06-17 17:47:39 -0700

    use less stocks, todo: dont use actor per node

--- bopm/src/main/scala/remote/MyActorApp.scala
+++ bopm/src/main/scala/remote/MyActorApp.scala
@@ -51,42 +51,43 @@ object MyActorApp extends App  {
 			"initial" -> 86.51,
 			"volatility" -> 0.154314,
 			"divYield" -> 0.0356
-		),
-		"CVX" -> Map(
-			"initial" -> 99.95,
-			"volatility" -> 0.119051,
-			"divYield" -> 0.0428
-		),
-		"CSCO" -> Map(
-			"initial" -> 28.78,
-			"volatility" -> 0.12598,
-			"divYield" -> 0.0292
-		),
-		"DIS" -> Map(
-			"initial" -> 111.05,
-			"volatility" -> 0.09465,
-			"divYield" -> 0.0104
-		),
-		"DD" -> Map(
-			"initial" -> 69.19,
-			"volatility" -> 0.133633,
-			"divYield" -> 0.0283
-		),
-		"XOM" -> Map(
-			"initial" -> 84.53,
-			"volatility" -> 0.097007,
-			"divYield" -> 0.0346
-		),
-		"GE" -> Map(
-			"initial" -> 27.19,
-			"volatility" -> 0.095077,
-			"divYield" -> 0.0338
-		),
-		"GS" -> Map(
-			"initial" -> 212.85,
-			"volatility" -> 0.126293,
-			"divYield" -> 0.0122
 		)
+		// ),
+		// "CVX" -> Map(
+		// 	"initial" -> 99.95,
+		// 	"volatility" -> 0.119051,
+		// 	"divYield" -> 0.0428
+		// ),
+		// "CSCO" -> Map(
+		// 	"initial" -> 28.78,
+		// 	"volatility" -> 0.12598,
+		// 	"divYield" -> 0.0292
+		// ),
+		// "DIS" -> Map(
+		// 	"initial" -> 111.05,
+		// 	"volatility" -> 0.09465,
+		// 	"divYield" -> 0.0104
+		// ),
+		// "DD" -> Map(
+		// 	"initial" -> 69.19,
+		// 	"volatility" -> 0.133633,
+		// 	"divYield" -> 0.0283
+		// ),
+		// "XOM" -> Map(
+		// 	"initial" -> 84.53,
+		// 	"volatility" -> 0.097007,
+		// 	"divYield" -> 0.0346
+		// ),
+		// "GE" -> Map(
+		// 	"initial" -> 27.19,
+		// 	"volatility" -> 0.095077,
+		// 	"divYield" -> 0.0338
+		// ),
+		// "GS" -> Map(
+		// 	"initial" -> 212.85,
+		// 	"volatility" -> 0.126293,
+		// 	"divYield" -> 0.0122
+		// )
 	)
 
 	var finished = 0
